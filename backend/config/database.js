23ecf311c86f2a9f0c495c4f1d000ff034910e55
Alexander <madfox@laserflare.net>
2022-05-16 13:23:09 +0300

    Update strapi db config

--- backend/config/database.js
+++ backend/config/database.js
@@ -2,8 +2,13 @@ const path = require('path');
 
 module.exports = ({ env }) => ({
   connection: {
-    client: 'sqlite',
+    client: 'mysql',
     connection: {
+      host: env('DATABASE_HOST'),
+      port: env('DATABASE_PORT'),
+      user: env('DATABASE_USER'),
+      password: env('DATABASE_PASSWORD'),
+      database: env('DATABASE_NAME'),
       filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
     },
     useNullAsDefault: true,
